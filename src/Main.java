64768abeb0d308515dbcc846889a53fc57799cac
Michał Nabielec <michal.nabie@gmail.com>
2019-04-03 12:04:42 +0200

    Modify - Main class Added - program logic to atBash and Baconian cipher

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args){
 
+        Baconian b = new Baconian();
+        AtBash aB = new AtBash();
+
+
+        if( args[0].equals("-e") && args[1].equals("baconian")) {
+            System.out.println(b.baconianCipherCodder(args));
+        }
+        else if ( args[0].equals("-d") && args[1].equals("baconian")) {
+            System.out.println(b.baconianCipherDecodder(args[2]));
+        }
+        else if ( args[0].equals("-e") && args[1].equals("atBash")) {
+            System.out.println(aB.atBashCodder(args));
+        }
+        else if ( args[0].equals("-d") && args[1].equals("atBash")) {
+            System.out.println(aB.atBashDecodder(args[2]));
+        }
     }
 }
