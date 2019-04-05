f331f8776dbe74a1106aa3a60fc4b336bca61716
Michał Nabielec <michal.nabie@gmail.com>
2019-04-05 09:22:39 +0200

    Modify - Main class Added option to display the available program options

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Baconian b = new Baconian();
         AtBash aB = new AtBash();
@@ -7,32 +7,24 @@ public class Main {
         cezarc c = new cezarc();
 
 
-        if( args[0].equals("-e") && args[1].equals("baconian")) {
+        if (args[0].equals("-e") && args[1].equals("baconian")) {
             System.out.println(b.baconianCipherCodder(args));
-        }
-        else if ( args[0].equals("-d") && args[1].equals("baconian")) {
+        } else if (args[0].equals("-d") && args[1].equals("baconian")) {
             System.out.println(b.baconianCipherDecodder(args[2]));
-        }
-        else if ( args[0].equals("-e") && args[1].equals("atBash")) {
+        } else if (args[0].equals("-e") && args[1].equals("atBash")) {
             System.out.println(aB.atBashCodder(args));
-        }
-        else if ( args[0].equals("-d") && args[1].equals("atBash")) {
+        } else if (args[0].equals("-d") && args[1].equals("atBash")) {
             System.out.println(aB.atBashDecodder(args[2]));
-        }
-
-
-        else if( args[0].equals("-e") && args[1].equals("xor")) {
+        } else if (args[0].equals("-e") && args[1].equals("xor")) {
             System.out.println(x.cipherCoder(args));
-        }
-        else if ( args[0].equals("-d") && args[1].equals("xor")) {
+        } else if (args[0].equals("-d") && args[1].equals("xor")) {
             System.out.println(x.cipherDecoder(args));
-        }
-
-        else if( args[0].equals("-e") && args[1].equals("caesar")) {
+        } else if (args[0].equals("-e") && args[1].equals("caesar")) {
             System.out.println(c.cipherCoder(args));
-        }
-        else if ( args[0].equals("-d") && args[1].equals("caesar")) {
+        } else if (args[0].equals("-d") && args[1].equals("caesar")) {
             System.out.println(c.cipherDecoder(args));
+        } else if (args[0].equals("-l")) {
+        System.out.println("[-e(encrypt)/-d(decrypt)] ['baconian/atBash/xor/caesar'] ['Text']");
         }
 
     }
